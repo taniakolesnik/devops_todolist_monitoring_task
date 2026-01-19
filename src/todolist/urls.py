bd79fc44f031b23097e678d35a3d8877ab48ba80
Tania Kolesnik <kolesnik@gmail.com>
2026-01-19 14:05:18 +0000

    deleted 2

--- src/todolist/urls.py
+++ src/todolist/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("api/", include("api.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path("admin/", admin.site.urls),
-    path("metrics", metrics),
+    path("metrics/", metrics),
 ]
